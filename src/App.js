ac6284805b8c74f9b231efa930eb8a5a89c15c84
Mersini Saif <genie.civil.mersini@gmail.com>
2020-12-04 02:50:45 +0100

    Testing App purposes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 
-import Formulaire from './components/Form'
-import Navbar from './components/Navbar'
-import Carousell from './components/Carousel'
-import './App.css';
+import React from "react";
+import './Style.css';
+import Pico from "./imageInSrc.jpg";
+// import Pica from "/imageInPublic.jpg";
 
 function App() {
   return (
-    <div className="App bg-dark">
-      <Navbar/>
-      <Carousell/>
-    <Formulaire/>
-    
+    <div className="App">
+     <div style={{border:"solid 1px black",maxWidth:'100vw'}}>
+
+        <h1 className={"title red"}>Your name here</h1><br/>        
+        <img className={"img1"} src={Pico} alt="img"/><br/>        
+        <img className={"img2"} src="/img/imageInPublic.jpg" alt="img"/>
+      </div>
+        <video style={{marginLeft:"25%",width:"50%",height:640}}controls>
+          <source src={"myVideo.mp4"} type={"video/mp4"}/>
+        </video>    
     </div>
   );
 }
